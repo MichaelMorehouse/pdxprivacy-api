6d449e40c8f6967184f7bc915f8ce2b459c1732e
mmdevelops <mmdevelops@gmail.com>
2019-04-01 14:03:05 -0700

    refactor api calls

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,27 +1,41 @@
-'use strict'
-var mongoose = require('mongoose'),
+const mongoose = require('mongoose'),
 	User = mongoose.model('User')
 
+exports.signup = function (req, res) {
+	var newUser = new User(req.body)
+	newUser.save()
+		.then(user => res.json(user))
+		.catch(err => res.send(err))
+}
+
+exports.login = function (req, res) {
+	User.findOne({ email: req.body.email })
+		.then(user => {
+			user.verifyPassword(req.body.password, function (err, isMatch) {
+				if (err) res.send(err)
+				res.send(isMatch)
+			})
+		})
+		.catch(err => res.send(err))
+}
+
 exports.listAll = function (req, res) {
-	User.find({}, function (err, user) {
-		if (err) res.send(err)
-		res.json(user)
-	})
+	User.find({})
+		.then(users => res.json(users))
+		.catch(err => res.send(err))
 }
 
 exports.create = function (req, res) {
 	var newUser = new User(req.body)
-	newUser.save(function (err, user) {
-		if (err) res.send(err)
-		res.json(user)
-	})
+	newUser.save()
+		.then(user => res.json(user))
+		.catch(err => res.send(err))
 }
 
 exports.read = function (req, res) {
-	User.findById(req.params.userId, function (err, user) {
-		if (err) res.send(err)
-		res.json(user)
-	})
+	User.findById(req.params.userId)
+		.then(user => res.json(user))
+		.catch(err => res.send(err))
 }
 
 exports.update = function (req, res) {
@@ -30,21 +44,15 @@ exports.update = function (req, res) {
 	},
 	req.body, {
 		new: true
-	},
-	function (err, user) {
-		if (err) res.send(err)
-		res.json(user)
 	})
+		.then(user => res.json(user))
+		.catch(err => res.send(err))
 }
 
 exports.delete = function (req, res) {
 	User.remove({
 		_id: req.params.userId
-	},
-	function (err, user) {
-		if (err) res.send(err)
-		res.json({
-			message: 'User successfully deleted'
-		})
 	})
+		.then(user => res.json({ message: 'User successfully deleted' }))
+		.catch(err => res.send(err))
 }
