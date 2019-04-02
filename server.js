8043d7349d41b4845b4dd7bc8ad92bb2630672ea
mmdevelops <mmdevelops@gmail.com>
2019-04-01 17:48:07 -0700

    updates for ezauth

--- server.js
+++ server.js
@@ -1,39 +1,52 @@
-var express = require('express'),
+require('dotenv').config()
+const express = require('express'),
 	mongoose = require('mongoose'),
 	bodyParser = require('body-parser'),
-	passport = require('passport'),
-	userModel = require('./api/models/userModel'),
-	blogPostModel = require('./api/models/blogPostModel')
+	morgan = require('morgan'),
+	// cipher = require('./services/cipher').cipherService,
+	// decipher = require('./services/cipher').decipherService,
+	routes = require('./routes')
 
-// Express initilization
-var app = express()
-// mongoose configuration
-mongoose.Promise = global.Promise
+// Load mongoose models
+require('./api/models/userModel'),
+require('./api/models/blogPostModel')
 
-// Middlewares
+let app = express()
+
+// NOTE: If your app is intended to perform or handle cross-origin
+// requests, run `npm install cors` and uncomment the code below
+// Additional configuration may be required, see
+// https://www.npmjs.com/package/cors
+// const cors = require('cors')
+// app.use(cors())
+
+// Register middleware
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(morgan('combined'))
 
-// app.use(function (req, res) {
-//     res.status(404).send({
-//         url: req.originalUrl + ' not found'
-//     })
-// })
+// Use decipher/cipher cervice, also comment/uncommend app.use(cipher) below
+// app.use(decipher)
 
-// Route registration
-
-var routes = require('./api/routes')
 routes(app)
+
+// app.use(cipher)
 
 // Pull connection string from env var or 
 // fallback to local db during development
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/pdxp'
+// eslint-disable-next-line no-undef
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/ezauth'
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 8080
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
-	.then(db => {
+	.then(() => {
+		// eslint-disable-next-line no-console
 		console.log('Connected to database')
 		app.listen(PORT)
+		// eslint-disable-next-line no-console
 		console.log(`App listening on port ${PORT}`)
 	})
+	// eslint-disable-next-line no-console
 	.catch(err => console.log(err))
+
